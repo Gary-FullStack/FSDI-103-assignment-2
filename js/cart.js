96980a6d14bd7652a26274a822181dd693e6ac95
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-10 19:47:30 -0500

    final version
    as good as I can make it

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,23 @@
+
 let total = 0;
-function addcart(price) {
+
+function addCart(price) {
     total = total + price;
     console.log(total);
     return total;
 }
+
+function calculateTaxes() {
+    subtotal = addCart(2);
+
+    let totalTax = subtotal * 1.11;
+
+    document.getElementById
+        ("receipt").innerHTML =
+        `<p<Subtotal = ${subtotal}</p>
+    <p> Total = ${totalTax}</p>`;
+
+}
+
+
+
